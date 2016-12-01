c5910b180de938bc48f3b0b1ae0e616272b7db79
Régis Hanol <regis@hanol.fr>
2016-12-01 13:41:57 +0100

    day 1

---
+++ 2016/day01.rb
@@ -0,0 +1,22 @@
+instructions = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4"
+
+directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+face = 0
+position = [0, 0]
+visited = { position => true }
+found = false
+
+instructions.split(", ").each do |instruction|
+  face = (face + (instruction[0]["R"] ? 1 : -1)) % 4
+  instruction[1..-1].to_i.times do
+    position = [position, directions[face]].transpose.map { |a| a.reduce(:+) }
+    if visited[position] && !found
+      puts "Part 2: #{position.map(&:abs).reduce(:+)}"
+      found = true
+    end
+    visited[position] = true
+  end
+end
+
+puts "Part 1: #{position.map(&:abs).reduce(:+)}"
