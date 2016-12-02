1b1f0aea2b9b6baee15d1e9650b41a65941f849a
Régis Hanol <regis@hanol.fr>
2016-12-02 11:48:50 +0100

    clearer code

--- 2016/day02.rb
+++ 2016/day02.rb
@@ -7,11 +7,11 @@
 # position = [1, 1]
 
 keypad = [
-  ["X", "X", "1", "X", "X"],
-  ["X", "2", "3", "4", "X"],
+  [nil, nil, "1", nil, nil],
+  [nil, "2", "3", "4", nil],
   ["5", "6", "7", "8", "9"],
-  ["X", "A", "B", "C", "X"],
-  ["X", "X", "D", "X", "X"],
+  [nil, "A", "B", "C", nil],
+  [nil, nil, "D", nil, nil],
 ]
 
 position = [2, 0]
@@ -29,10 +29,10 @@ instructions = File.read("day02.input").strip
 instructions.split("\n").each do |line|
   line.chars do |d|
     new_position = [
-      [0, [position[0] + directions[d][0], max].min].max,
-      [0, [position[1] + directions[d][1], max].min].max,
+      [0, [max, position[0] + directions[d][0]].min].max,
+      [0, [max, position[1] + directions[d][1]].min].max,
     ]
-    position = new_position unless keypad[new_position[0]][new_position[1]] == "X"
+    position = new_position if keypad[new_position[0]][new_position[1]]
   end
   putc keypad[position[0]][position[1]]
 end
