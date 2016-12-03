0aa80a5aa66205137131fb7b2a8e9b1b2046ad1d
Régis Hanol <regis@hanol.fr>
2016-12-03 16:05:50 +0100

    shorter version for day3

--- 2016/day03.rb
+++ 2016/day03.rb
@@ -1,13 +1,7 @@
-triangles = 0
-
-input = File.readlines("day03.input")
-
-input.map { |l| l.split.map(&:to_i) }
-     .transpose # remove for part 1
-     .flatten
-     .each_slice(3) do |t|
-  a, b, c = t
-  triangles += 1 if (a + b > c) && (b + c > a) && (a + c > b)
-end
-
-puts triangles
+p File.readlines("day03.input")
+      .map { |l| l.split.map(&:to_i) }
+      .transpose # remove for part 1
+      .flatten
+      .each_slice(3)
+      .flat_map
+      .count { |a, b, c| a + b > c && b + c > a && a + c > b }
