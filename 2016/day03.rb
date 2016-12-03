9c79c6f57920ed2dd41a321914926aa5fef7b7a5
Régis Hanol <regis@hanol.fr>
2016-12-03 16:08:40 +0100

    even shorter version for day 3

--- 2016/day03.rb
+++ 2016/day03.rb
@@ -1,7 +1,4 @@
-p File.readlines("day03.input")
-      .map { |l| l.split.map(&:to_i) }
-      .transpose # remove for part 1
-      .flatten
-      .each_slice(3)
-      .flat_map
-      .count { |a, b, c| a + b > c && b + c > a && a + c > b }
+    p File.readlines("day03.input")
+          .map { |l| l.split.map(&:to_i) }
+          .transpose.flatten.each_slice(3) # remove for part 1
+          .count { |a, b, c| a + b > c && b + c > a && a + c > b }
