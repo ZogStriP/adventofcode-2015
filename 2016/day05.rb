472aba49ab704b94b7db587af8fb36254617546f
Régis Hanol <regis@hanol.fr>
2016-12-05 09:01:12 +0100

    day 5

---
+++ 2016/day05.rb
@@ -0,0 +1,15 @@
+require "digest"
+
+h = nil
+index = 0
+door_id = "reyedfim"
+
+part_2 = Array.new(8)
+
+while part_2.any?(&:nil?) do
+  until (h = Digest::MD5.hexdigest("#{door_id}#{index += 1}"))[/^0{5}/] do; end
+  putc h[5] # hacking in progress...
+  part_2[h[5].to_i] ||= h[6] if ("0".."7") === h[5]
+end
+
+puts "", part_2.join
