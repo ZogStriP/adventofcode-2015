5ade45a00ecccd1a8561e61971a50ac841ca3370
Régis Hanol <regis@hanol.fr>
2016-12-09 14:24:33 +0100

    use '$&' to retrieve regex matched text

--- 2016/day09.rb
+++ 2016/day09.rb
@@ -1,6 +1,6 @@
 def decompress(file, deep = false)
-  return file.size unless marker = file =~ /(\((\d+)x(\d+)\))/
-  size, chars, times = $1.size, $2.to_i, $3.to_i
+  return file.size unless marker = file =~ /\((\d+)x(\d+)\)/
+  size, chars, times = $&.size, $1.to_i, $2.to_i
   offset = marker + size
   f = file[offset...offset + chars]
   marker + (deep ? decompress(f, deep) : f.size) * times + decompress(file[offset + chars..-1], deep)
