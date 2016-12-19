043f5c9feea0dd3db5932ae1d007eb8de644e887
Régis Hanol <regis@hanol.fr>
2016-12-19 13:06:45 +0100

    very slightly shorter day 19

--- 2016/day19.rb
+++ 2016/day19.rb
@@ -1,6 +1,6 @@
 # https://en.wikipedia.org/wiki/Josephus_problem#k.3D2
 def last_elf_next(n)
-  2 * (n - 2 ** (n.to_s(2).size - 1)) + 1
+  2 * (n.to_s(2)[1..-1].to_i(2)) + 1
 end
 
 def last_elf_across(n, p = 1)
