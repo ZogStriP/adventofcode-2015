6f2b9b90b4909791fb46941312dee00bb39ea393
Régis Hanol <regis@hanol.fr>
2016-12-19 12:16:56 +0100

    faster day 19

--- 2016/day19.rb
+++ 2016/day19.rb
@@ -1,10 +1,11 @@
-def last_elf_next(elfs)
-  (2..elfs).reduce(0) { |e, n| (e + 2) % n } + 1
+# https://en.wikipedia.org/wiki/Josephus_problem#k.3D2
+def last_elf_next(n)
+  2 * (n - 2 ** (n.to_s(2).size - 1)) + 1
 end
 
-def last_elf_across(elfs, e = 1)
-  e *= 3 while e * 3 <= elfs
-  e == elfs ? e : elfs - e + [elfs - 2 * e, 0].max
+def last_elf_across(n, p = 1)
+  p = 3 ** Math.log(n, 3).to_i
+  p == n ? n : n - p + [n - 2 * p, 0].max
 end
 
 ELFS = 3004953
