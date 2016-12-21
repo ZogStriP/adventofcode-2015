915160445d1ee3e8259e6058dee63657ee4d8a90
Régis Hanol <regis@hanol.fr>
2016-12-21 11:09:23 +0100

    array rather than string

--- 2016/day21.rb
+++ 2016/day21.rb
@@ -1,22 +1,22 @@
 instructions = File.readlines("day21.input")
 
-password = "abcdefgh"
+password = "abcdefgh".chars
 instructions.each do |line|
   case line
   when /swap position (\d+) with position (\d+)/
     x, y = $1.to_i, $2.to_i
     password[x], password[y] = password[y], password[x]
   when /swap letter (\w) with letter (\w)/
-    password.tr!("#{$2}#{$1}", "#{$1}#{$2}")
+    password = password.map { |c| c == $1 ? $2 : c == $2 ? $1 : c }
   when /rotate (left|right) (\d+) steps?/
-    password = password.chars.rotate(($1["left"] ? 1 : -1) * $2.to_i).join
+    password.rotate!(($1["left"] ? 1 : -1) * $2.to_i)
   when /rotate based on position of letter (\w)/
     i = password.index($1)
     rotation = -(i < 4 ? i + 1 : i + 2)
-    password = password.chars.rotate(rotation).join
+    password.rotate!(rotation)
   when /reverse positions (\d+) through (\d+)/
     x, y = $1.to_i, $2.to_i
-    password = password[0...x] + password[x..y].reverse + password[y + 1..-1]
+    password[x..y] = password[x..y].reverse
   when /move position (\d+) to position (\d+)/
     x, y = $1.to_i, $2.to_i
     letter = password.slice!(x)
@@ -24,25 +24,25 @@ instructions.each do |line|
   end
 end
 
-p password
+p password.join
 
-password = "fbgdceah"
+password = "fbgdceah".chars
 instructions.reverse.each do |line|
   case line
   when /swap position (\d+) with position (\d+)/
     x, y = $1.to_i, $2.to_i
     password[x], password[y] = password[y], password[x]
   when /swap letter (\w) with letter (\w)/
-    password.tr!("#{$2}#{$1}", "#{$1}#{$2}")
+    password = password.map { |c| c == $1 ? $2 : c == $2 ? $1 : c }
   when /rotate (left|right) (\d+) steps?/
-    password = password.chars.rotate(($1["left"] ? -1 : 1) * $2.to_i).join # diff
+    password.rotate!(($1["right"] ? 1 : -1) * $2.to_i) # diff
   when /rotate based on position of letter (\w)/
     i = password.index($1)
     rotation = i / 2 + (i % 2 == 1 || i == 0 ? 1 : 5) # diff
-    password = password.chars.rotate(rotation).join
+    password.rotate!(rotation)
   when /reverse positions (\d+) through (\d+)/
     x, y = $1.to_i, $2.to_i
-    password = password[0...x] + password[x..y].reverse + password[y + 1..-1]
+    password[x..y] = password[x..y].reverse
   when /move position (\d+) to position (\d+)/
     x, y = $2.to_i, $1.to_i # diff
     letter = password.slice!(x)
@@ -50,4 +50,4 @@ instructions.reverse.each do |line|
   end
 end
 
-p password
+p password.join
