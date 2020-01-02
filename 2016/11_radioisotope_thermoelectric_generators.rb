61215ca76b8bbceb1358eb215414e2e62ed2abc6
Régis Hanol <regis@hanol.fr>
2020-01-02 10:20:32 +0100

    restart 2016 for fun

--- 2016/11_radioisotope_thermoelectric_generators.rb
+++
@@ -1,4 +0,0 @@
-# (2 * n - 3) steps to move n items from one level to another [where n >= 3]
-
-p (1...4).reduce(0) { |sum, x| sum + 2 * [8, 2, 0, 0].take(x).reduce(:+) - 3 }
-p (1...4).reduce(0) { |sum, x| sum + 2 * [12, 2, 0, 0].take(x).reduce(:+) - 3 }
--- 2016/16_dragon_checksum.rb
+++
@@ -1,7 +0,0 @@
-[272, 35651584].each { |size|
-  d = "10001001100000001"
-  d += "0" + d.reverse.tr("01", "10") while d.size < size
-  d = d[0...size].chars
-  d = d.each_slice(2).map { |a, b| a == b ? "1" : "0" } while d.size.even?
-  p d.join
-}
