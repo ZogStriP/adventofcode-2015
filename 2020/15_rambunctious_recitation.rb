39b25af38800cad18c81fde528fbd5ce4706a5dc
Régis Hanol <regis@hanol.fr>
2020-12-15 16:50:28 +0100

    less lines

--- 2020/15_rambunctious_recitation.rb
+++ 2020/15_rambunctious_recitation.rb
@@ -1,14 +1,13 @@
-T = []
+last  = 0
+turns = []
 
 init = DATA.read.split(?,).map(&:to_i)
-init.each.with_index(1) { |n, i| T[n] = i }
-
-last = 0
+init.each.with_index(1) { |n, i| turns[n] = i }
 
 (init.size + 1...30000000).each { |t|
   p last if t == 2020
-  tt = T[last]
-  T[last] = t
+  tt = turns[last]
+  turns[last] = t
   last = t - (tt || t)
 }
 
