c10b85d54cdb25cccad7f2ae342309d62075ccad
Régis Hanol <regis@hanol.fr>
2020-12-17 14:08:02 +0100

    even shorter & genericer day 17

--- 2020/17_conway_cubes.rb
+++ 2020/17_conway_cubes.rb
@@ -1,9 +1,8 @@
-ddd, dddd = {}, {}
+init = {}
 
 DATA.each_line.with_index { |l, y|
   l.each_char.with_index { |c, x|
-    ddd[[x, y, 0]] = true if c[?#]
-    dddd[[x, y, 0, 0]] = true if c[?#]
+    init[[x, y]] = true if c[?#]
   }
 }
 
@@ -14,23 +13,18 @@ def neighbors(c)
   @diffs[c.size].each { |d| yield c.zip(d).map(&:sum) }
 end
 
-6.times {
-  counts, _copy = Hash.new(0), {}
-  ddd.each_key { |c| neighbors(c) { |n| counts[n] += 1 } }
-  counts.each { |c, n| _copy[c] = true if (ddd[c] && n == 2) || n == 3 }
-  ddd = _copy
+[3, 4].each { |d|
+  space = init.transform_keys { |c| c + [0] * (d - 2) }
+
+  6.times {
+    counts, _copy = Hash.new(0), {}
+    space.each_key { |c| neighbors(c) { |n| counts[n] += 1 } }
+    counts.each { |c, n| _copy[c] = true if (space[c] && n == 2) || n == 3 }
+    space = _copy
+  }
+
+  p space.size
 }
-
-p ddd.size
-
-6.times {
-  counts, _copy = Hash.new(0), {}
-  dddd.each_key { |c| neighbors(c) { |n| counts[n] += 1 } }
-  counts.each { |c, n| _copy[c] = true if (dddd[c] && n == 2) || n == 3 }
-  dddd = _copy
-}
-
-p dddd.size
 
 __END__
 #.#..#.#
